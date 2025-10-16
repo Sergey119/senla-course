21f943c867d70bfe70046d68fef364e87ba0b091
Sergey119 <manymail08@gmail.com>
2025-10-16 20:53:11 +0300

    completing task 3

---
+++ task-3/4/src/Person.java
@@ -0,0 +1,9 @@
+public abstract class Person {
+    protected int id;
+    protected String name;
+
+    public int getId() { return id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+}
